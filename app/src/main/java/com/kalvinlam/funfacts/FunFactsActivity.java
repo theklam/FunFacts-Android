5274df49cc031bd6512931e311d2f6815dda9ff6
Kalvin Lam <kalvin.lam5@gmail.com>
2016-05-09 14:52:20 -0700

    App finished

--- app/src/main/java/com/kalvinlam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/kalvinlam/funfacts/FunFactsActivity.java
@@ -2,13 +2,14 @@ package com.kalvinlam.funfacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
     // Declare our View variables
@@ -41,6 +42,7 @@ public class FunFactsActivity extends AppCompatActivity {
         };
         mShowFactButton.setOnClickListener(listener);
 
-        Toast.makeText(FunFactsActivity.this, "Yay! Our activity was created!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(FunFactsActivity.this, "Yay! Our activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d("FunFactsActivity", "We're logging from the onCreate() method!");
     }
 }
