e152685c20ea682ea40663310b561995eba0d4de
Kalvin Lam <kalvin.lam5@gmail.com>
2016-05-09 12:06:58 -0700

    Background changes color on every click

--- app/src/main/java/com/kalvinlam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/kalvinlam/funfacts/FunFactsActivity.java
@@ -1,16 +1,20 @@
 package com.kalvinlam.funfacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     // Declare our View variables
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +24,17 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assign the Views from the layout files to the corresponding variables
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                String color = mColorWheel.getColor();
 
                 // Update TextView to show this new fact
-                mFactTextView.setText(mFactBook.getFact());
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
 
             }
         };
